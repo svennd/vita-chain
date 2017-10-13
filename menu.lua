8284c4c47cb6b8b2243c3ad7caa591b36eec8847
Svennd <svennson@gmail.com>
2017-10-13 16:58:26 +0200

    wip

--- menu.lua
+++ menu.lua
@@ -4,6 +4,9 @@
 local DISPLAY_WIDTH = 960
 local DISPLAY_HEIGHT = 544
 
+-- menu constants
+local MENU = {START = 1, HELP = 2, EXIT = 3, MIN = 1, MAX = 3}
+
 -- color definitions
 local white 	= Color.new(255, 255, 255)
 local black 	= Color.new(0, 0, 0)
@@ -13,6 +16,10 @@ local background = Graphics.loadImage("app0:/assets/bg.png")
 
 -- load font
 local main_font = Font.load("app0:/assets/ArchivoNarrow-Regular.ttf")
+
+-- menu vars
+local oldpad = SCE_CTRL_RTRIGGER -- input init
+local current_menu = 0 -- menu position
 
 -- draw function
 function draw()
@@ -47,8 +54,8 @@ function draw()
 	Graphics.fillRect(
 		598,
 		880,
-		78,
-		161,
+		182,
+		254,
 		Color.new(234,182,143))
 	Font.print(main_font, 645, 193, "HELP", white)
 		
@@ -56,20 +63,53 @@ function draw()
 	Graphics.fillRect(
 		598,
 		880,
-		78,
-		161,
+		273,
+		345,
 		Color.new(234,182,143))
 	Font.print(main_font, 645, 284, "EXIT", white)
-		
+	
+	
+	-- if pad is used for controll draw that
+	if current_menu ~= 0 then
+		if current_menu == 1 then
+			Graphics.fillRect(598, 880,
+				78,
+				161,
+				white)
+		elseif current_menu == 2 then
+			Graphics.fillRect(598, 880,
+				182,
+				254,
+				white)
+		elseif current_menu == 3 then
+			Graphics.fillRect(598, 880,
+				273,
+				345,
+				white)
+		end
+	end
+	
 	Graphics.termBlend()
 	Screen.flip()
 end
 
-local oldpad = SCE_CTRL_RTRIGGER
+-- draw a box
+-- untill fillEmptyRect is fixed
+function draw_box(x1, x2, y1, y2, width, color)
 
-MENU = {START = 1, HELP = 2, EXIT = 3, MIN = 1, MAX = 3}
-
-current_menu = 0
+	-- top line
+	Graphics.fillRect(x1, x2+width, y1, y1+width, color)
+	
+	-- bot line
+	Graphics.fillRect(x1, x2+width, y2, y2+width, color)
+	
+	-- left line
+	Graphics.fillRect(x1, x1+width, y1, y2, color)
+	
+	-- right line
+	Graphics.fillRect(x2, x2+width, y1, y2, color)
+	
+end
 
 function user_input()
 	local pad = Controls.read()
